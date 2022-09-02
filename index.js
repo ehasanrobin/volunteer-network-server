80501d3c4cb9a09f7ac4257f1140a68a8c0c6ae5
ehasanrobin <ehasanrobin44@gmail.com>
2022-09-02 23:47:32 +0600

    commit

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const port =  process.env.PORT || 5000
 const cors = require('cors')
-require('dotenv').config()
-// middleware 
+
+// middlewaree 
 app.use(cors())
 app.use(express.json())
 
